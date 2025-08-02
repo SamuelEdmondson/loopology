f5e652d5db7aa19c610f1f471275d50db7fa6642
Benton Edmondson <bentonedmondson@gmail.com>
2025-08-02 17:31:27 -0400

    Gravity experimentation

--- ship.gd
+++ ship.gd
@@ -1,7 +1,7 @@
 extends RigidBody2D
 
-@export var G = 10000
-@export var rotation_correction = 1000
+@export var G = 1000000
+@export var asteroid_field = 80
 @onready var mesh = $MeshInstance2D
 
 func _ready() -> void:
@@ -11,12 +11,8 @@ func _process(delta: float) -> void:
 	var superposition = calculate_gravity_superposition()
 	self.apply_force(superposition)
 	
-	mesh.global_rotation = self.linear_velocity.angle() - PI/2
-	#var offset = Vector2(cos(self.rotation), sin(self.rotation)).angle_to(self.linear_velocity)
-	#print(offset)
-	#offset *= rotation_correction
-	## never negative
-	#self.apply_torque(offset)
+	self.rotation = self.linear_velocity.angle() - PI/2
+	
 	
 func calculate_gravity_superposition() -> Vector2:
 	var asteroids = get_tree().get_nodes_in_group("Asteroids")
@@ -29,6 +25,12 @@ func calculate_gravity_with_asteroid(asteroid: Node) -> Vector2:
 	var m1: float = asteroid.mass
 	var m2: float = self.mass
 	var r = self.position.distance_to(asteroid.position)
-	var gravity_scalar = (G * m1 * m2) / (r * r)
+	
+	var gravity_scalar = (G * m1 * m2) / (r*r)
+	if r < asteroid_field:
+		r = asteroid_field
+		gravity_scalar = (G * m1 * m2) / (r*r)
+	
+	#var gravity_scalar = (G * m1 * m2) / (r * r)
 	var unit_vector = (asteroid.position - self.position).normalized()
 	return unit_vector * gravity_scalar
